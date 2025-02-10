2b31b83324cc1de3c1520172fd02f88a9d010c4c
Shirley Domonique <shirledo@il-i2.msk.21-school.ru>
2025-02-10 13:54:50 +0300

    change chat_id for tg_bot

--- src/ex06/analytics.py
+++ src/ex06/analytics.py
@@ -9,19 +9,24 @@ logging.basicConfig(
     format='%(asctime)s %(message)s',  # Формат логов
     datefmt='%Y-%m-%d %H:%M:%S'  # Формат даты и времени
 )
+
+
 def check_delim(str):
-    if str[0].rstrip()!=str[0] or str[1].lstrip()!=str[1]:
+    if str[0].rstrip() != str[0] or str[1].lstrip() != str[1]:
         return True
     else:
         return False
+
+
 class Research():
-    def __init__(self,path):
-        self.path=path
+    def __init__(self, path):
+        self.path = path
         logging.info(f"Initialized Research with path: {path}")
+
     def send_telegram_message(self, message):
         logging.info(f"Sending messege to Telegram: {message}")
         bot_token = "6344201451:AAHotPq3VhRWFHkhgJfEnidvVxidWlSIkUs"
-        chat_id = "856446462"      
+        chat_id = "-1002292565577"
         url = f"https://api.telegram.org/bot{bot_token}/sendMessage"
         params = {
             "chat_id": chat_id,
@@ -32,61 +37,71 @@ class Research():
             if response.status_code == 200:
                 logging.info("Message sent to Telegram successfully.")
             else:
-                logging.error(f"Failed to send message to Telegram. Status code: {response.status_code}")
+                logging.error(
+                    f"Failed to send message to Telegram. Status code: {response.status_code}")
         except Exception as e:
             logging.error(f"Error sending message to Telegram: {e}")
-    def check_the_file(self, has_header = True):
+
+    def check_the_file(self, has_header=True):
         logging.info("Checking the file for correct format.")
-        with open(self.path,'r') as file:
-                lines = file.readlines()
-                if has_header:
-                    start = 1
-                    if len(lines)<2:
-                        raise ValueError("File must contain at least two lines: header and data.")
-                    header =lines[0].split(",")
-                    if len(header)!=2:
-                        raise ValueError("Header have wrong format.")
-                    if check_delim(header):
-                        raise ValueError("Header have wrong delimiter.")
-                else :
-                    start = 0
-                    if len(lines)<1:
-                        raise ValueError("File must contain at least one line of data.")
-                for line in lines[start:]:
-                    data=line.split(",")
-                    if len(data) !=2:
-                        raise ValueError("Each data line must contain only two values")
-                    if check_delim(data):
-                        raise ValueError("Data have wrong delimiter.")
-                    if int(data[0]) not in [0,1] or int(data[1]) not in [0,1]:
-                        raise ValueError("Data values must be 0 or 1.")
-                    if int(data[1]) + int(data[0]) != 1:
-                        raise ValueError("Data values must be 0,1 or 1,0.")
-    def file_reader(self, has_header = True):
+        with open(self.path, 'r') as file:
+            lines = file.readlines()
+            if has_header:
+                start = 1
+                if len(lines) < 2:
+                    raise ValueError(
+                        "File must contain at least two lines: header and data.")
+                header = lines[0].split(",")
+                if len(header) != 2:
+                    raise ValueError("Header have wrong format.")
+                if check_delim(header):
+                    raise ValueError("Header have wrong delimiter.")
+            else:
+                start = 0
+                if len(lines) < 1:
+                    raise ValueError(
+                        "File must contain at least one line of data.")
+            for line in lines[start:]:
+                data = line.split(",")
+                if len(data) != 2:
+                    raise ValueError(
+                        "Each data line must contain only two values")
+                if check_delim(data):
+                    raise ValueError("Data have wrong delimiter.")
+                if int(data[0]) not in [0, 1] or int(data[1]) not in [0, 1]:
+                    raise ValueError("Data values must be 0 or 1.")
+                if int(data[1]) + int(data[0]) != 1:
+                    raise ValueError("Data values must be 0,1 or 1,0.")
+
+    def file_reader(self, has_header=True):
         logging.info("Reading the file.")
         if has_header:
-            start=1
+            start = 1
         else:
             start = 0
         if (os.path.exists(self.path)):
             self.check_the_file(has_header)
-            with open(self.path,'r') as file:
-                lists = [[int(item) for item in line.strip().split(",")] for line in file.readlines()[start:]]
+            with open(self.path, 'r') as file:
+                lists = [[int(item) for item in line.strip().split(",")]
+                         for line in file.readlines()[start:]]
                 return lists
         else:
             raise ValueError("No such file or directory")
+
     class Calculations():
         def __init__(self, data):
             self.data = data
             logging.info(f"Initialized Calculations.")
-        def counts(self,data):
+
+        def counts(self, data):
             logging.info("Calculating counts of heads and tails.")
-            heads=0
-            tails=0
+            heads = 0
+            tails = 0
             for line in data:
-                heads+=line[0]
-                tails+=line[1]
+                heads += line[0]
+                tails += line[1]
             return heads, tails
+
         def fractions(self):
             logging.info("Calculating fractions of heads and tails.")
             heads, tails = self.counts(self.data)
@@ -96,6 +111,7 @@ class Research():
             f1 = heads/sum * 100
             f2 = tails/sum * 100
             return f1, f2
+
     class Analytics(Calculations):
         def __init__(self, data):
             super().__init__(data)
@@ -111,11 +127,12 @@ class Research():
                 else:
                     predict.append([1, 0])
             return predict
+
         def predict_last(self):
             logging.info("Getting the last prediction.")
             return self.data[-1]
-        def save_file(self,data, name, format):
+
+        def save_file(self, data, name, format):
             logging.info(f"Saving data to {name}.{format}.")
             with open(f"{name}.{format}", 'w') as export:
                 export.write(data)
-    
--- src/ex06/make_report.py
+++ src/ex06/make_report.py
@@ -2,15 +2,16 @@ from analytics import Research, logging
 from config import num_of_steps, report_template
 import sys
 
-try:   
-    res=Research(sys.argv[1])
+try:
+    res = Research(sys.argv[1])
     data = res.file_reader()
     observations = len(data)
-    analitics=res.Analytics(data)
+    analitics = res.Analytics(data)
     heads, tails = analitics.counts(data)
     heads_percent, tails_percent = analitics.fractions()
-    predicted_heads,predicted_tails = analitics.counts(analitics.predict_random(num_of_steps))
-    report=report_template.format(
+    predicted_heads, predicted_tails = analitics.counts(
+        analitics.predict_random(num_of_steps))
+    report = report_template.format(
         observations=observations,
         tails=tails,
         heads=heads,
@@ -20,9 +21,11 @@ try:
         predicted_tails=predicted_tails,
         predicted_heads=predicted_heads
     )
-    analitics.save_file(report,"report","txt")
+    analitics.save_file(report, "report", "txt")
     logging.info("Report successfully created.")
     res.send_telegram_message("The report has been successfully created.")
 except Exception as e:
     logging.error(f"Error: {e}")
-    res.send_telegram_message("The report hasn't been created due to an error.")
+    res.send_telegram_message(
+        "The report hasn't been created due to an error.")
+    res.send_telegram_message(f"Error: {e}")
